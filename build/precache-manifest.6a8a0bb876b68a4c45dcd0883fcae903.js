561818e0ce1f93aaca98ca80d892b0d0ed820f6c
sivaprakash <sivaprakash.r@greatinnovus.com>
2019-02-15 18:49:02 +0530

    Initial

---
+++ build/precache-manifest.6a8a0bb876b68a4c45dcd0883fcae903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sampleApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4fe401d8ce1444c052ee",
+    "url": "/sampleApp/static/js/runtime~main.4fe401d8.js"
+  },
+  {
+    "revision": "b5e8b2250dec69eff472",
+    "url": "/sampleApp/static/js/main.b5e8b225.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/sampleApp/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "b5e8b2250dec69eff472",
+    "url": "/sampleApp/static/css/main.ab7db80f.chunk.css"
+  },
+  {
+    "revision": "0eee627576439d4a18008235a7ec16dd",
+    "url": "/sampleApp/index.html"
+  }
+];
